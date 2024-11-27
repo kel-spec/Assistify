927d8733281b53c5cd205e3733254d3e91b77c69
kel-spec <qmlflagunday@tip.edu.ph>
2024-11-27 09:26:21 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -122,4 +122,5 @@ elif st.session_state["user_role"] == "developer":
         st.write(f"Username: {user}, Role: {info['role']}")
     
     if st.button("Sign Out"):
-        st.session_state["user_role"] = None
+    st.session_state["user_role"] = None
+    st.session_state["current_user"] = None
